71bc2899ae7ed27a45149a77bf39ce715cb31cdf
James Livengood <james.livengood@gmail.com>
2018-03-22 15:13:15 -0400

    grid and node logic

--- bundle.js
+++ bundle.js
@@ -71,6 +71,18 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./lib/dfsBuildMaze.js":
+/*!*****************************!*\
+  !*** ./lib/dfsBuildMaze.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n\nclass dfsBuildMaze {\n  constructor(grid) {\n    this.grid = grid;\n    this.array = grid.array;\n  }\n\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (dfsBuildMaze);\n\n//# sourceURL=webpack:///./lib/dfsBuildMaze.js?");
+
+/***/ }),
+
 /***/ "./lib/entry.js":
 /*!**********************!*\
   !*** ./lib/entry.js ***!
@@ -79,7 +91,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _grid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./grid */ \"./lib/grid.js\");\n\n\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n  const canvasLeft = document.getElementsByTagName(\"canvas\")[0];\n  const canvasRight = document.getElementsByTagName(\"canvas\")[1];\n  canvasLeft.width = 450;\n  canvasLeft.height = 450;\n  canvasRight.width = 450;\n  canvasRight.height = 450;\n  const ctxLeft = canvasLeft.getContext(\"2d\");\n  const ctxRight = canvasRight.getContext(\"2d\");\n\n  // ctxLeft.fillStyle = 'blue';\n  // ctxLeft.fillRect(0, 147, 12, 6);\n  // ctxLeft.fillRect(0, 0, 6, 6);\n  // ctxLeft.fillRect(294, 147, 12, 6);\n  //\n  // ctxRight.beginPath();\n  // ctxRight.moveTo(0, 0);\n  // ctxRight.lineTo(425, 425);\n  // ctxRight.strokeStyle = \"red\";\n  // ctxRight.stroke();\n});\n\n//# sourceURL=webpack:///./lib/entry.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _grid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./grid */ \"./lib/grid.js\");\n/* harmony import */ var _util_draw_grid_util__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./util/draw_grid_util */ \"./lib/util/draw_grid_util.js\");\n/* harmony import */ var _dfsBuildMaze__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./dfsBuildMaze */ \"./lib/dfsBuildMaze.js\");\n\n\n\n\ndocument.addEventListener(\"DOMContentLoaded\", () => {\n  const canvasLeft = document.getElementsByTagName(\"canvas\")[0];\n  const canvasRight = document.getElementsByTagName(\"canvas\")[1];\n  canvasLeft.width = 464;\n  canvasLeft.height = 464;\n  canvasRight.width = 464;\n  canvasRight.height = 464;\n  const ctxLeft = canvasLeft.getContext(\"2d\");\n  const ctxRight = canvasRight.getContext(\"2d\");\n\n  // debugger\n  const leftGrid = new _grid__WEBPACK_IMPORTED_MODULE_0__[\"default\"]([28, 0]);\n  leftGrid.populateGrid();\n  const rightGrid = new _grid__WEBPACK_IMPORTED_MODULE_0__[\"default\"]([28, 28]);\n  rightGrid.populateGrid();\n  Object(_util_draw_grid_util__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(ctxLeft, leftGrid);\n  Object(_util_draw_grid_util__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(ctxRight, rightGrid);\n\n  // ctxLeft.fillStyle = 'blue';\n  // ctxLeft.fillRect(0, 448, 8, 8);\n\n  // ctxLeft.fillStyle = 'blue';\n  // ctxLeft.fillRect(0, 147, 12, 6);\n  // ctxLeft.fillRect(0, 0, 6, 6);\n  // ctxLeft.fillRect(294, 147, 12, 6);\n  //\n  // ctxRight.beginPath();\n  // ctxRight.moveTo(0, 0);\n  // ctxRight.lineTo(425, 425);\n  // ctxRight.strokeStyle = \"red\";\n  // ctxRight.stroke();\n});\n\n//# sourceURL=webpack:///./lib/entry.js?");
 
 /***/ }),
 
@@ -91,7 +103,31 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _gri
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n\n\nclass Grid {\n  constructor(props) {}\n\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Grid);\n\n//# sourceURL=webpack:///./lib/grid.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _node__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node */ \"./lib/node.js\");\n\n\nclass Grid {\n  constructor(root_pos) {\n    this.array = [...Array(29)].map(i => Array(29));\n    this.populateGrid = this.populateGrid.bind(this);\n    this.root_pos = root_pos;\n  }\n\n  populateGrid() {\n    // debugger\n    for (var i = 0; i < this.array.length; i++) {\n      for (var j = 0; j < this.array[i].length; j++) {\n        this.array[i][j] = new _node__WEBPACK_IMPORTED_MODULE_0__[\"default\"]([j, i], null);\n      }\n    }\n    this.array[this.root_pos[0]][this.root_pos[1]].parent = 'root';\n  }\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Grid);\n\n//# sourceURL=webpack:///./lib/grid.js?");
+
+/***/ }),
+
+/***/ "./lib/node.js":
+/*!*********************!*\
+  !*** ./lib/node.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n\n\nclass Node {\n  constructor(coords, parent) {\n    this.coords = coords;\n    this.parent = parent;\n    this.x = coords[0];\n    this.y = coords[1];\n  }\n\n  distance() {\n    if (this.parent) {\n      if (this.parent === 'root') {\n        return 0;\n      } else {\n        return this.parent.distance() + 1;\n      }\n    } else {\n      return nil;\n    }\n  }\n\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Node);\n\n//# sourceURL=webpack:///./lib/node.js?");
+
+/***/ }),
+
+/***/ "./lib/util/draw_grid_util.js":
+/*!************************************!*\
+  !*** ./lib/util/draw_grid_util.js ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n\n\nconst DrawGridUtil = (ctx, grid) => {\n  // debugger\n  grid.array.map(row => {\n    row.map(node => {\n      ctx.strokeStyle = 'rgb(75,\t55,\t23)';\n      ctx.lineWidth = 0.5;\n      return ctx.strokeRect(node.x * 16, node.y * 16, 16, 16);\n    });\n  });\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (DrawGridUtil);\n\n//# sourceURL=webpack:///./lib/util/draw_grid_util.js?");
 
 /***/ })
 
